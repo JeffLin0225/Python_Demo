58563ddfcf359bfcc51790fd14d5bebf763992de
jeff <jefflin0225@gmail.com>
2025-04-04 01:52:40 +0800

    新增chromadb持久化

--- VectorDataBase/ChromaDB.py
+++ VectorDataBase/ChromaDB.py
@@ -3,31 +3,39 @@ import chromadb
 使用向量資料庫，原廠預設使用： all-MiniLM-L6-v2 模型 
 """
 
-""" 初始化 ChromaDB 客戶端 """
-chroma_client = chromadb.Client()
+""" 初始化 ChromaDB 客戶端 [記憶體模式]"""
+# chroma_client = chromadb.Client()
 
-""" 創建集合，並指定使用自定義嵌入函數 """
-testCollect = chroma_client.create_collection(
+chroma_client = chromadb.PersistentClient("./chroma_data")
+
+""" 創建集合 """
+testCollect = chroma_client.get_or_create_collection(
     name="testCollect",
 )
 
 """ 添加數據 """
-# documents 主要內容 [必填] (會被轉為向量)
-# metadatas 補充內容 [選填] (不會被轉為向量) [key:value]
-# ids 主鍵 [必填]
-testCollect.add(
-    documents=["get_weather: 獲取城市天氣", "search_web: 搜索網頁", "get_location: 獲取城市位置"],
-    metadatas=[
-        {"name": "get_weather", "description": "獲取城市天氣"},
-        {"name": "search_web", "description": "搜索網頁"},
-        {"name": "get_location", "description": "獲取城市位置"}
-    ],
-    ids=["fun1", "fun2", "fun3"],
-)
+"""
+    documents 主要內容 [必填] (會被轉為向量)
+    metadatas 補充內容 [選填] (不會被轉為向量) [key:value]
+    ids 主鍵 [必填]
+"""
+if testCollect.count() == 0 :
+    testCollect.add(
+      documents=["get_weather: 獲取城市天氣", "search_web: 搜索網頁", "get_location: 獲取城市位置"],
+      metadatas=[
+          {"name": "get_weather", "description": "獲取城市天氣"},
+          {"name": "search_web", "description": "搜索網頁"},
+          {"name": "get_location", "description": "獲取城市位置"}
+      ],
+      ids=["fun1", "fun2", "fun3"],
+    )
+    print("collection 裡面還沒有資料")
+else :
+    print("collection 已經有資料了")
 
 """ 查詢 """
 results = testCollect.query(
-    query_texts=["台灣在哪裡"],
+    query_texts=["ˇ中國在哪邊？"],
     n_results=1,  # 找 Top-1
 )
 
